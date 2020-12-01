8a1d50f8761484fa0b90ef95017a36c3afafb167
Vid Čufar <vid.cufar@gmail.com>
2020-12-01 16:24:30 +0100

    Added: lodash dependency & Utils simplification

--- src/engine/Utils.ts
+++ src/engine/Utils.ts
@@ -1,25 +1,15 @@
+import { clone } from 'lodash';
+import Entity from './Entity';
+import Camera from './Camera';
+import Model from './Model';
+import { IEntityOptions } from '../types';
+
 export default class Utils {
   static init(
-    object: any,
-    defaults: Record<string, any>,
-    options: Record<string, any>,
+    object: Entity | Camera | Model,
+    defaults: IEntityOptions,
+    options: IEntityOptions,
   ): void {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const filtered = Utils.clone(options || {});
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const defaulted = Utils.clone(defaults || {});
-    for (const key in filtered) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access,no-prototype-builtins
-      if (!defaulted.hasOwnProperty(key)) {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-        delete filtered[key];
-      }
-    }
-    Object.assign(object, defaulted, filtered);
-  }
-
-  static clone(object: any): any {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    return JSON.parse(JSON.stringify(object));
+    Object.assign(object, clone(defaults || {}), clone(options || {}));
   }
 }
