95ccef9dea4be67bbd94449ac47d8fe024a4d5bd
Vid Čufar <vid.cufar@gmail.com>
2020-12-24 01:04:35 +0100

    Updated: SVG object scale & color

--- src/assets/svg/pyramid.ts
+++ src/assets/svg/pyramid.ts
@@ -1,11 +1,11 @@
 // language=SVG
 export const Pyramid = `
 <?xml version='1.0' encoding='ascii'?>
-<svg xmlns="http://www.w3.org/2000/svg" height="48" version="1.1" width="48">
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="48" height="48">
   <g id="View Layer_LineSet">
     <g id="strokes">
-      <path d=" M 35.396, 28.932 28.131, 22.061 23.951, 18.108 16.609, 24.897 12.617, 28.589 21.032, 33.991 23.550, 35.606 32.262, 30.698 35.396, 28.932 " fill="none" stroke="rgb(0, 0, 0)" stroke-linecap="butt" stroke-linejoin="round" stroke-opacity="1.0" stroke-width="1.0" />
-      <path d=" M 23.951, 18.108 23.722, 28.106 23.550, 35.606 " fill="none" stroke="rgb(0, 0, 0)" stroke-linecap="butt" stroke-linejoin="round" stroke-opacity="1.0" stroke-width="1.0" />
+      <path class="scale-1" d=" M 35.396, 28.932 28.131, 22.061 23.951, 18.108 16.609, 24.897 12.617, 28.589 21.032, 33.991 23.550, 35.606 32.262, 30.698 35.396, 28.932 " fill="white" stroke="rgb(0, 0, 0)" stroke-linecap="butt" stroke-linejoin="round" stroke-opacity="1.0" stroke-width="1.0" />
+      <path class="scale-1" d=" M 23.951, 18.108 23.722, 28.106 23.550, 35.606 " fill="none" stroke="rgb(0, 0, 0)" stroke-linecap="butt" stroke-linejoin="round" stroke-opacity="1.0" stroke-width="1.0" />
     </g>
   </g>
 </svg>
